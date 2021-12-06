760b2f4b0a5177895fbb68760fbfd175b00783dc
vitalichatyrka <vitalichatyrka@gmail.com>
2021-12-06 11:06:21 +0300

    master

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,32 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args) {
         String URL = "https://s5.casino.softswiss.com/ru";
-        String passToWebdriver = "C:\\Users\\vitali.chatyrka\\Downloads\\selenium-java-4.1.0\\chromedriver.exe";
+        String pathToWebdriver = "C:\\Users\\vitali.chatyrka\\Downloads\\selenium-java-4.1.0\\chromedriver.exe";
+        String password = "1234567Qa!";
+        String email = "player.user.ss+301292@gmail.com";
+        String cssSelectorOfSignInButton = "#mm-0 > div.main > header > div > div.header-user-block.col-lg-5.col-md-5.col-sm-5.col-xs-6 > div > div:nth-child(1) > div > a.login_lnk.ng-scope";
+        String xpathOfInputEmail = "//*[@id=\"email\"]";
+        String xpathOfInputPassword = "//*[@id=\"password\"]";
+        String cssSelectorOfsignInButton = "body > div.modal.fade.ng-isolate-scope.modal-login.in > div > div > div > div:nth-child(4) > div:nth-child(5) > input";
+        String cssSelectorOfUserName = "#mm-0 > div.main > header > div > div.header-user-block.col-lg-5.col-md-5.col-sm-5.col-xs-6 > div > div.auth-user > div.user > div.user__name.ng-binding";
 
-
-        System.setProperty("webdriver.chrome.driver",passToWebdriver);
+        System.setProperty("webdriver.chrome.driver",pathToWebdriver);
         ChromeDriver newDriver = new ChromeDriver();
         newDriver.get(URL);
 
-        WebElement signInButton = newDriver.findElement(By.cssSelector("#mm-0 > div.main > header > div > div.header-user-block.col-lg-5.col-md-5.col-sm-5.col-xs-6 > div > div:nth-child(1) > div > a.login_lnk.ng-scope"));
+        WebElement signInButton = newDriver.findElement(By.cssSelector(cssSelectorOfSignInButton));
         signInButton.click();
 
-        WebElement inputEmail = newDriver.findElement(By.xpath("//*[@id=\"email\"]"));
-        inputEmail.sendKeys("player.user.ss+301292@gmail.com");
+        WebElement inputEmail = newDriver.findElement(By.xpath(xpathOfInputEmail));
+        inputEmail.sendKeys(email;
 
-        WebElement inputPassword = newDriver.findElement(By.xpath("//*[@id=\"password\"]"));
-        inputPassword.sendKeys("1234567Qa!");
+        WebElement inputPassword = newDriver.findElement(By.xpath(xpathOfInputPassword));
+        inputPassword.sendKeys(password);
 
-        WebElement button = newDriver.findElement(By.cssSelector("body > div.modal.fade.ng-isolate-scope.modal-login.in > div > div > div > div:nth-child(4) > div:nth-child(5) > input"));
-        button.click();
+        WebElement LoginButton = newDriver.findElement(By.cssSelector(cssSelectorOfsignInButton));
+        LoginButton.click();
 
-        WebElement successTest = newDriver.findElement(By.cssSelector("#mm-0 > div.main > header > div > div.header-user-block.col-lg-5.col-md-5.col-sm-5.col-xs-6 > div > div.auth-user > div.user > div.user__name.ng-binding"));
+        WebElement successTest = newDriver.findElement(By.cssSelector(cssSelectorOfUserName));
         System.out.println("Test passed");
 
 
